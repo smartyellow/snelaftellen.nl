c40a137b4bca6dfc00a618167452ea1d0b8831ab
Romein van Buren <romein@vburen.nl>
2021-12-29 17:42:03 +0100

    CO2-badge naar deurmat
    Signed-off-by: Romein van Buren <romein@vburen.nl>

--- src/lib/doormat.svelte
+++ src/lib/doormat.svelte
@@ -1,13 +1,21 @@
 <footer>
 	<nav>
 		<ul>
-			<li>&copy; {new Date().getFullYear()}</li>
+			<li>&copy; {new Date().getFullYear()} SnelAftellen.nl</li>
 			<li><a href="/contact">Contact</a></li>
 			<li><a href="/doc/privacy.pdf">Privacy</a></li>
 			<li><a href="/doc/cookies.pdf">Cookies</a></li>
 			<li><a href="/doc/disclaimer.pdf">Disclaimer</a></li>
 		</ul>
 	</nav>
+
+	<noscript>
+		<a href="https://www.websitecarbon.com/website/snelaftellen-nl/">
+			Deze website is groener dan 98% van de websites.
+		</a>
+	</noscript>
+	<div id="wcb" class="carbonbadge"></div>
+	<script src="https://unpkg.com/website-carbon-badges@1.1.3/b.min.js" defer></script>
 </footer>
 
 <style>
@@ -16,6 +24,9 @@
 		padding: 1rem;
 		text-align: center;
 		color: #ffffff;
+	}
+	footer > * {
+		margin-bottom: 10px;
 	}
 	ul {
 		list-style-type: none;
