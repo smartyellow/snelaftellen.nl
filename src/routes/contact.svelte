af9f2a21a5c5a491fa61066c27fe58c4d057b252
Romein van Buren <romein@vburen.nl>
2022-02-18 14:12:58 +0100

    Contactpagina
    Signed-off-by: Romein van Buren <romein@vburen.nl>

---
+++ src/routes/contact.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Meta from "$lib/molecules/layout/meta.svelte";
+</script>
+
+<Meta
+	title="Contact - SnelAftellen.nl"
+	description="Heb je een suggestie of vraag voor SnelAftellen.nl? Stuur hier een berichtje!"
+/>
+
+<h1>Neem contact op</h1>
+
+<p>
+	Heb je een suggestie of vraag voor SnelAftellen.nl? Stuur een mailtje naar
+	<a href="mailto:mail@snelaftellen.nl">mail@snelaftellen.nl</a>, dan proberen
+	we zo snel mogelijk te antwoorden. Dank je wel!
+</p>
+
+<p>
+	PS. Ben je benieuwd hoe SnelAftellen.nl van binnen werkt? Kijk even op de
+	<a href="/over" sveltekit:prefetch>Over-pagina</a>!
+</p>
