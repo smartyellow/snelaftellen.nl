e131ef1200e0ec8b3c201753cc86f19786fb3f12
Romein van Buren <romein@vburen.nl>
2022-03-19 20:46:54 +0100

    Kalender meta
    Signed-off-by: Romein van Buren <romein@vburen.nl>

--- src/routes/kalender.svelte
+++ src/routes/kalender.svelte
@@ -5,7 +5,10 @@
 	export let year = new Date().getFullYear();
 </script>
 
-<Meta title="Jaarkalender {year} van SnelAftellen.nl" />
+<Meta
+	title="Jaarkalender van {year} bekijken op SnelAftellen.nl"
+	description="Bekijk hier de jaarkalender van het jaar {year}. Zoek snel dagen en weeknummers op."
+/>
 
 <h1>Kies een datum van de kalender</h1>
 
