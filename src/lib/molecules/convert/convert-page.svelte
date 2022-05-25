d5040a87b820e20753206cfaec2db0736330e33e
Romein van Buren <romein@vburen.nl>
2022-05-25 08:52:19 +0200

    SEO op omrekenpagina
    Signed-off-by: Romein van Buren <romein@vburen.nl>

--- src/lib/molecules/convert/convert-page.svelte
+++ src/lib/molecules/convert/convert-page.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import Convert from "./convert.svelte";
 	import ConvertTable from "./convert-table.svelte";
+	import Meta from "$lib/layout/meta.svelte";
 </script>
 
 <h1>Omrekenen</h1>
+
+<Meta
+	title="Eenheden omrekenen op SnelAftellen.nl"
+	description="Reken hier alle tijdseenheden naar elkaar om. Van seconden naar jaren, van maanden naar milliseconden en van dagen naar uren."
+/>
 
 <Convert />
 <ConvertTable />
